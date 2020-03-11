88f747430bf9f876d5cf754d16a7f0cfe9a6f207
Baltemos <baltemos@gmail.com>
2020-03-11 11:27:33 +0100

    Hashmap implementation

--- bot/src/main/java/Commands/PingCommand.java
+++ bot/src/main/java/Commands/PingCommand.java
@@ -3,14 +3,13 @@ package Commands;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import org.jetbrains.annotations.NotNull;
+import net.dv8tion.jda.api.entities.Activity.Timestamps;
 
 public class PingCommand extends Command{
     @Override
     public void execute(GuildMessageReceivedEvent event) {
         MessageChannel channel = event.getChannel();
         long time = System.currentTimeMillis();
-        channel.sendMessage("Pong: " + time).queue(response->{
-            response.editMessageFormat("Pong: %d ms", (System.currentTimeMillis() - time));
-        });
+        channel.sendMessage("Pong: " + event.getResponseNumber()).queue();
     }
 }
