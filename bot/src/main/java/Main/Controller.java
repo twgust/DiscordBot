c72c6b538389c9b5f32db359d076a3ca0b70db0a
Baltemos <baltemos@gmail.com>
2020-04-15 12:11:48 +0200

    token fix

--- bot/src/main/java/Main/Controller.java
+++ bot/src/main/java/Main/Controller.java
@@ -27,7 +27,7 @@ public class Controller {
 
     public Controller() throws LoginException, IOException {
         token = new Token();
-        JDA jda = new JDABuilder("Njc3ODY4NjM4OTAwMDYwMTYx.XpbTtA.LhAuQ50SYMW5TfG36CJ_AnSGMX4").build();
+        JDA jda = new JDABuilder(token.getToken()).build();
         waiter = new EventWaiter();
         quizCommand = new QuizCommand();
         jda.addEventListener(new EventListener(this));
