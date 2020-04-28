66d6d3ba6fb7ca68772bc09899e75762d42657c1
twgust <gustavzfloren@gmail.com>
2020-04-28 10:37:43 +0200

    committoanime
    a

--- bot/src/main/java/MusicModule/MusicCommands/MusicQueueCommand.java
+++ bot/src/main/java/MusicModule/MusicCommands/MusicQueueCommand.java
@@ -3,7 +3,10 @@ package MusicModule.MusicCommands;
 import Commands.Command;
 import MusicModule.*;
 
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+import java.util.Iterator;
 
 public class MusicQueueCommand extends Command {
   private MusicController controller;
@@ -14,9 +17,20 @@ public class MusicQueueCommand extends Command {
   }
 
   @Override
-    public void execute(GuildMessageReceivedEvent event){
-      event.getChannel().sendMessage("```Current queue: " + controller.getScheduler().getQueue().size()
-      + "``` ... More commands related to queue coming").queue();
+  public void execute(GuildMessageReceivedEvent event){
+    Iterator<AudioTrack> itr = controller.getScheduler().getQueue().iterator();
+    String str = "\nCurrent Queue: " + controller.getScheduler().getQueue().size() + "\n";
 
+    while(itr.hasNext()){
+      str += itr.next().getInfo().title +"\n";
+    }
+
+    event.getChannel().sendMessage("```"
+            + str
+            + "\nNow playing: " + controller.getPlayer().getPlayingTrack().getInfo().title
+            + "\nLink: " + controller.getPlayer().getPlayingTrack().getInfo().uri
+            + "``` ... More commands related to queue coming").queue();
   }
 }
+
+
