8ea693dc9154e95d3ec6b56eb792d22c8dd1dbec
twgust <gustavzfloren@gmail.com>
2020-06-02 14:52:39 +0200

    Updated formatting

--- bot/src/main/java/MusicModule/MusicCommands/MusicHelp.java
+++ bot/src/main/java/MusicModule/MusicCommands/MusicHelp.java
@@ -15,13 +15,13 @@ public class MusicHelp extends Command {
         embedBuilder.setColor(Color.YELLOW);
         embedBuilder.setTitle("Music Module 🎶", "https://github.com/twgust/DiscordBot/tree/master/bot/src/main/java/MusicModule");
         embedBuilder.setDescription("https://github.com/twgust/");
-        embedBuilder.addField("%play <song>", "plays a song", true);
-        embedBuilder.addField("%skip", "skips", true);
-        embedBuilder.addField("%queue", "returns queue", true);
-        embedBuilder.addField("%song", "currently playing song", true);
-        embedBuilder.addField("%pause", "pauses the music", true);
-        embedBuilder.addField("%resume", "resumes the music", true);
-        embedBuilder.addField("%search <song>", "searches youtube and returns results", false);
+        embedBuilder.addField("play [song]", "plays a song", true);
+        embedBuilder.addField("skip", "skips", true);
+        embedBuilder.addField("queue", "returns queue", true);
+        embedBuilder.addField("song", "currently playing song", true);
+        embedBuilder.addField("pause", "pauses the music", true);
+        embedBuilder.addField("resume", "resumes the music", true);
+        embedBuilder.addField("search [song]    ", "searches youtube and returns results", false);
         embedBuilder.setImage("https://i.imgur.com/Z2DZ2p5.jpg");
         embedBuilder.setFooter("DM @Goose#0068 for support");
 
